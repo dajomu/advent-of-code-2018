cc14e1a2efa3d11d830730f246e33b45cf396dc5
Daniel Munday <mundaymeister@gmail.com>
2019-12-01 12:20:58 +0000

    day1 part2

--- day1/fuel-calculator/src/main.rs
+++ day1/fuel-calculator/src/main.rs
@@ -6,20 +6,45 @@ fn main() {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    first_part(contents);
+    second_part(contents);
 }
 
 fn first_part(contents: std::string::String) {
     let module_weights = contents.lines();
 
-    let total_fuel: i32 = module_weights.map(|mass| calculate_fuel(mass.parse::<f64>().unwrap())).sum();
+    let total_fuel: i32 = module_weights.map(|mass| calculate_fuel(mass.parse::<i32>().unwrap())).sum();
 
     println!("total fuel for modules: {}", total_fuel)
-    // total_fuel
 }
 
-fn calculate_fuel(weight: f64) -> (i32) {
+fn second_part(contents: std::string::String) {
+
+    let module_weights = contents.lines();
+
+    let total_fuel: i32 = module_weights.map(|mass| calculate_fuel_with_fuel(mass.parse::<i32>().unwrap())).sum();
+
+    println!("total fuel including fuel fuel: {}", total_fuel);
+}
+
+fn calculate_fuel(weight: i32) -> (i32) {
     //  mass, divide by three, round down, and subtract 2
-    let fuel = (weight / 3.0).floor() as i32 - 2;
+    let fuel = ((weight / 3) as f64).floor() as i32 - 2;
     fuel
+}
+
+fn calculate_fuel_with_fuel(weight: i32) -> (i32) {
+    let initial_fuel = calculate_fuel(weight);
+    recursive_fuel(initial_fuel, initial_fuel)
+}
+
+fn recursive_fuel(total_weight: i32, current_weight: i32) -> (i32) {
+    let fuel_needed = calculate_fuel(current_weight);
+    if fuel_needed > 0  
+     {  
+        recursive_fuel(total_weight + fuel_needed, fuel_needed)
+     }
+     else
+     {
+        total_weight
+     }
 }
