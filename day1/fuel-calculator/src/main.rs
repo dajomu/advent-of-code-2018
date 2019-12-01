862cc53ed0487bae9ec733f385bd10c783c4be87
Daniel Munday <mundaymeister@gmail.com>
2019-12-01 11:31:13 +0000

    first part of first challenge

--- day1/fuel-calculator/src/main.rs
+++ day1/fuel-calculator/src/main.rs
@@ -1,20 +1,25 @@
-use std::env;
 use std::fs;
 
 fn main() {
-
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Searching for {}", query);
-
-    println!("In file {}", filename);
+    let filename = "modules.txt";
 
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    first_part(contents);
+}
+
+fn first_part(contents: std::string::String) {
+    let module_weights = contents.lines();
+
+    let total_fuel: i32 = module_weights.map(|mass| calculate_fuel(mass.parse::<f64>().unwrap())).sum();
+
+    println!("total fuel for modules: {}", total_fuel)
+    // total_fuel
+}
+
+fn calculate_fuel(weight: f64) -> (i32) {
+    //  mass, divide by three, round down, and subtract 2
+    let fuel = (weight / 3.0).floor() as i32 - 2;
+    fuel
 }
